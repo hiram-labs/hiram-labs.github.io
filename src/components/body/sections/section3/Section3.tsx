6302a0b6ebddcc3e4e846a421a2c7e3f674fefc5
Nana C-Note <nana@Nanas-MacBook-Pro.local>
2020-09-25 17:31:48 +0100

    tech

--- src/components/body/sections/section3/Section3.tsx
+++ src/components/body/sections/section3/Section3.tsx
@@ -28,18 +28,23 @@ const Section3: React.FC<TProps> = (): JSX.Element => {
           </div>
           <div className={'self-start py-4 text-xl'}>{content.techSub}</div>
         </div>
-        <div className={'lg:mx-20 mx-10 mt-10 overflow-hidden'}>
+        <div
+          className={cx(
+            styles['tech-images'],
+            'lg:mx-20 mx-10 mt-10 overflow-hidden relative'
+          )}
+        >
           <div
             className={
               'w-full flex items-center space-x-16 pb-2 overflow-scroll'
             }
           >
             {content.tech.map((e, i, arr) => (
-              <div key={e.name}>
+              <div className={cx(i === arr.length - 1 && 'pr-32')} key={e.name}>
                 {React.createElement(
                   require('../../../../../dev/assets/images/tech/' + e.image),
                   {
-                    className: 'nav-logo w-20 sm:animate-grow'
+                    className: 'w-20 sm:animate-grow'
                   }
                 )}
               </div>
--- src/components/body/sections/section3/section3.module.css.d.ts
+++ src/components/body/sections/section3/section3.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "section3": string;
+  readonly "tech-images": string;
 };
 export = styles;
 
